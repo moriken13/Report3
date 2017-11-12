a8e52b1d7596b357ef99c32ab8ca760c1979ee40
Kenta Mori <e175713@ie.u-ryukyu.ac.jp>
2017-11-12 16:44:00 +0900

    Second

--- src/main/jp/ac/uryukyu/ie/e175713/Hero.java
+++ src/main/jp/ac/uryukyu/ie/e175713/Hero.java
@@ -8,7 +8,7 @@ package jp.ac.uryukyu.ie.e175713;
  *  boolean dead; //敵の生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
-public class Hero {
+public class Hero extends LivingThing {
     String name;
     int hitPoint;
     int attack;
@@ -21,45 +21,25 @@ public class Hero {
      * @param attack ヒーローの攻撃力
      */
     public Hero (String name, int maximumHP, int attack) {
+        super(name, maximumHP, attack);
         this.name = name;
         hitPoint = maximumHP;
         this.attack = attack;
         dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
     }
 
-    /**
-     * getterメソッドと同等。生死をboolean表現しているためメソッド名をisDead()とした。
-     * @return boolean
-     */
-    public boolean isDead(){
-        return  dead;
-    }
 
-    public String getName(){
-        return name;
-    }
-
-    /**
-     * Enemyへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param e 攻撃対象
-     */
-    public void attack(Enemy e){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
-        e.wounded(damage);
-    }
 
     /**
      * 自身へ攻撃されたときのダメージ処理をするメソッド。
      * 指定されたダメージを hitPoint から引き、死亡判定を行う。
      * @param damage 受けたダメージ
      */
+    @Override
     public void wounded(int damage){
         hitPoint -= damage;
         if( hitPoint < 0 ) {
-            dead = true;
+            super.dead = true;
             System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
         }
     }
